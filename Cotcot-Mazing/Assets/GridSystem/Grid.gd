80dcd3e32b7e1d18f16494979fa140ee65884142
nsobczak <nicolas.sobczak@orange.fr>
2019-02-15 01:20:03 +0100

    added features to grid

--- Cotcot-Mazing/Assets/GridSystem/Grid.gd
+++ Cotcot-Mazing/Assets/GridSystem/Grid.gd
@@ -8,9 +8,11 @@ export(String) var cellScenePath = "res://Assets/GridSystem/Cell/Cell.tscn"
 export(Vector2) var cellSize = Vector2(24, 24)
 export(float) var cellMargin = 4
 
-var _gridDic = {}
+var _gridPositionToReal = {}
+var _gridActorNamePositions = {}
 
 
+#_________________________________________________________________________________________
 func _generate():
 	var cellScene = load(self.cellScenePath)
 	
@@ -19,15 +21,19 @@ func _generate():
 			var cellPosition = Vector2((i+1) * self.cellSize.x, (j+1) * self.cellSize.y)
 #			print("cell ({0}, {1}) position is: ({2}, {3})" \
 #			.format([i, j, cellPosition.x, cellPosition.y]))
-			self._gridDic[Vector2(i, j)] = cellPosition
+			self._gridPositionToReal[Vector2(i, j)] = cellPosition
 			
 			var cellScene_instance = cellScene.instance()
-			cellScene_instance.set_name("cellScene_({0},{1})".format([i, j]))
+			cellScene_instance.set_name("cellScene_({0}.0,{1}.0)".format([i, j]))
+#			print("cellScene_({0}.0,{1}.0)".format([i, j]))
 			cellScene_instance.transform.origin = Vector3(cellPosition.x, 0, cellPosition.y)
 			cellScene_instance.setNature(EMPTY)
 			add_child(cellScene_instance)
 #			print(has_node("cellScene_({0},{1})".format([i, j]))) #prints True
-			
+
+func _initActorPositions():
+	for childNode in self.get_children():
+ 		self._gridActorNamePositions[childNode.name] = Vector2(0, 0)
 
 func _ready():
 	# Called when the node is added to the scene for the first time.	
@@ -35,11 +41,81 @@ func _ready():
 
 	# add cells instances from prefabs
 	_generate()
+	_initActorPositions()
 
-func getCellNature(coordinates):	
+
+#_________________________________________________________________________________________
+func findActorByName(actorName):
+	for childNode in self.get_children():
+		if (childNode.name == actorName):
+			return childNode
+	print("{0} wasn't found".format([actorName]))
+	return null
+
+func getCellNature(gridCoordinates):	
 	# get cell at those coordinates and use its getNature function
-	return self._gridDic[Vector2(coordinates.x, coordinates.y)].getNature()
+	var cellName = "cellScene_({0},{1})".format([gridCoordinates.x, gridCoordinates.y])
+	var cell = findActorByName(cellName)
+	if cell != null:
+		return cell.getNature()
+	else:
+		return null
 
+func canMoveToCell(gridCoordinates):
+	if (gridCoordinates in self._gridPositionToReal):
+		return (getCellNature(gridCoordinates) >= 0)
+	else:
+		return false
+
+func updateActorGridPosition(actorName, newGridPosition):
+	var actor = findActorByName(actorName)
+	if actor != null:
+		# update actor position in grid
+		self._gridActorNamePositions[actorName] = newGridPosition #Vector2()
+		# update actual actor position
+		var gridPosition = self._gridActorNamePositions[actorName]
+		var worldPosition = self._gridPositionToReal[gridPosition]
+		actor.transform.origin = Vector3(worldPosition.y, 0, worldPosition.x)
+
+
+#_________________________________________________________________________________________
+func moveActor(actorNameToMove, gridCoordinates):
+	if canMoveToCell(gridCoordinates):
+		updateActorGridPosition(actorNameToMove, gridCoordinates)
+	else:
+		print("can't move to that cell")
+
+func moveUp(actorNameToMove):
+	var currentCell = self._gridActorNamePositions[actorNameToMove]
+	var newCell = Vector2(currentCell.x, currentCell.y + 1)
+	if canMoveToCell(newCell):
+		updateActorGridPosition(actorNameToMove, newCell)
+	else:
+		print("can't move to cell above")
+		
+func moveRight(actorNameToMove):
+	var currentCell = self._gridActorNamePositions[actorNameToMove]
+	var newCell = Vector2(currentCell.x + 1, currentCell.y)
+	if canMoveToCell(newCell):
+		updateActorGridPosition(actorNameToMove, newCell)
+	else:
+		print("can't move to right cell")
+		
+func moveDown(actorNameToMove):
+	var currentCell = self._gridActorNamePositions[actorNameToMove]
+	var newCell = Vector2(currentCell.x, currentCell.y - 1)
+	if canMoveToCell(newCell):
+		updateActorGridPosition(actorNameToMove, newCell)
+	else:
+		print("can't move to cell underneath")
+
+func moveLeft(actorNameToMove):
+	var currentCell = self._gridActorNamePositions[actorNameToMove]
+	var newCell = Vector2(currentCell.x - 1, currentCell.y)
+	if canMoveToCell(newCell):
+		updateActorGridPosition(actorNameToMove, newCell)
+	else:
+		print("can't move to left cell")
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
--- Cotcot-Mazing/Assets/Player/PlayerController.gd
+++ Cotcot-Mazing/Assets/Player/PlayerController.gd
@@ -1,8 +1,7 @@
 extends KinematicBody
 
-# class member variables go here, for example:
-export var speed = 25
-export var direction = Vector3()
+onready var grid = get_parent()
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -11,21 +10,17 @@ func _ready():
 
 
 func _input(deltaVal):
-	direction = Vector3(0,0,0)
-	if Input.is_action_pressed("ui_left"):
-		direction.x -=1
-	if Input.is_action_pressed("ui_right") :
-		direction.x +=1
-	if Input.is_action_pressed("ui_up"):
-		direction.z -=1
-	if Input.is_action_pressed("ui_down"):
-		direction.z +=1
-
-
+	if Input.is_action_just_released("ui_left"):
+		self.grid.moveLeft(self.name)
+	if Input.is_action_just_released("ui_right") :
+		self.grid.moveRight(self.name)
+	if Input.is_action_just_released("ui_up"):
+		self.grid.moveUp(self.name)
+	if Input.is_action_just_released("ui_down"):
+		self.grid.moveDown(self.name)
 
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
-
 	_input(delta)
 	pass
